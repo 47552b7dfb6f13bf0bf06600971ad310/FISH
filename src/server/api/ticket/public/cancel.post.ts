d1c42639a6cf1ca9564ef6e6b17c00843c81ca59
Your Name <you@example.com>
2025-08-07 18:19:31 +0700

    Update

--- src/server/api/ticket/public/cancel.post.ts
+++ src/server/api/ticket/public/cancel.post.ts
@@ -6,13 +6,18 @@ export default defineEventHandler(async (event) => {
     const { code } = await readBody(event)
     if(!code) throw 'Không tìm thấy mã vé'
 
-    const ticket = await DB.Ticket.findOne({ code: code, user: auth._id }).select('cancel complete code spot') as IDBTicket
-    if(!ticket) throw 'Vé này không còn tồn tại'
+    const ticket = await DB.Ticket.findOne({ code: code, user: auth._id }).select('cancel status complete code spot') as IDBTicket
+    if(!ticket) throw 'Không tìm thấy dữ liệu vé câu'
     if(!!ticket.cancel) throw 'Vé này đã bị hủy'
-    if(!!ticket.complete.pay.pending) throw 'Không thể hủy vé câu đã thanh toán'
+    if(ticket.status > 0) throw 'Không thể hủy vé câu đã thanh toán'
 
-    ticket.cancel = true
-    await ticket.save()
+    await DB.Ticket.updateOne({ _id: ticket._id }, {
+      complete: {
+        cancel: auth._id
+      },
+      cancel: true
+    })
+
     await DB.LakeSpot.updateOne({ _id: ticket.spot }, { status: 0 })
 
     return resp(event, { message: `Đã hủy vé ${ticket.code}` })
--- src/server/api/ticket/staff/cancel.post.ts
+++ src/server/api/ticket/staff/cancel.post.ts
@@ -3,28 +3,25 @@ import type { IAuth, IDBTicket } from "~~/types"
 export default defineEventHandler(async (event) => {
   try {
     const auth = await getAuth(event) as IAuth
-    if(auth.type < 3) throw 'Bạn không có quyền thao tác'
+    if(auth.type < 1) throw 'Bạn không có quyền thao tác'
 
     const { code } = await readBody(event)
     if(!code) throw 'Không tìm thấy mã vé'
 
-    const ticket = await DB.Ticket.findOne({ code: code }).select('cancel complete code spot') as IDBTicket
+    const ticket = await DB.Ticket.findOne({ code: code }).select('cancel complete code spot status') as IDBTicket
     if(!ticket) throw 'Vé này không còn tồn tại'
     if(!!ticket.cancel) throw 'Vé này đã bị hủy'
 
-    const end = Date.now()
+    if(ticket.status > 0 && auth.type < 3) throw 'Bạn không có quyền thao tác hủy với vé đã thanh toán'
+
     await DB.Ticket.updateOne({ _id: ticket._id }, {
-      end: end,
       complete: {
-        time: true,
         cancel: auth._id
       },
-      cancel: true,
-      status: 2
+      cancel: true
     })
 
     await DB.LakeSpot.updateOne({ _id: ticket.spot }, { status: 0 })
-
     return resp(event, { message: `Thao tác thành công` })
   } 
   catch (e:any) {
